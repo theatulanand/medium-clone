69fec9360f913137ead50fa10a2dce07d112861c
AnkitAnkushe <ankitankushe@gmail.com>
2022-08-03 15:58:17 +0530

    home page done

--- src/components/Home_BL/Home.jsx
+++ src/components/Home_BL/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import './HomeBL.css'
 import Typography from '@mui/material/Typography';
@@ -7,7 +7,8 @@ import Login from '../gAuth/Login';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import EmailIcon from '@mui/icons-material/Email';
 import Box from '@mui/material/Box';
-import { Skeleton } from '@mui/material';
+import { Avatar, Skeleton } from '@mui/material';
+import { useEffect } from 'react';
 const style = {
   position: 'absolute',
   top: '50%',
@@ -24,6 +25,38 @@ const Home = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [data,setData] = useState([])
+  const [isloading,setLoading] = useState(true)
+
+
+  useEffect(() => {
+    handleFetch()
+  },[])
+
+  const handleFetch = () => {
+    setLoading(true)
+    fetch(`http://localhost:8080/blogs`)
+    .then((res) => res.json())
+    .then((res) => {
+      setData(res);
+      setLoading(false)
+    })
+  }
+
+    var months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
 
   return (
     <div>
@@ -31,9 +64,9 @@ const Home = () => {
         <Navbar />
         <div className='Home_BL_Banner'>
           <div style={{ paddingLeft: '120px' }}>
-            <h1 style={{ fontSize: "100px", fontWeight: "500" }}>Stay curious.</h1>
+             <h1 style={{ fontSize: "100px", fontWeight: "500" }}>Stay curious.</h1>
             <p style={{ fontSize: "30px", fontWeight: "500", color: "black" }}>
-              Discover stories, thinking, and expertise
+             Discover stories, thinking, and expertise
               <br />
               from writers on any topic.
             </p>
@@ -57,9 +90,32 @@ const Home = () => {
 
       <div className='home_bl_bottom'>
         <div>
-          <Skeleton variant="text" />
-          <Skeleton variant="circular" width={40} height={40} />
-          <Skeleton variant="rectangular" width={210} height={118} />
+          {
+            isloading?<>
+            <Skeleton variant="text" />
+            <Skeleton variant="circular" width={40} height={40} />
+            <Skeleton variant="rectangular" width={210} height={118} />
+            </>: 
+            data.map((el) => (
+              <div style={{display:"grid",gridTemplateColumns:"2fr 1fr",marginTop:"50px"}}>
+                <div>
+                <div style={{display:"flex"}}>
+                <Avatar alt="Remy Sharp" src={el.avatar} /> <h5>{el.author}</h5>
+              </div>
+              <div>
+                <h3>{el.title}</h3>
+              </div>
+              <div className='post-details-div' style={{color:"grey"}}>
+                <span>{Math.floor(Math.random()*31)+1}{months[Math.floor(Math.random()*months.length)]} </span>
+                <span>{Math.floor(Math.random()*10)+1} min read</span>
+              </div>
+              </div>
+              <div>
+                <img style={{width:"180px",height:"130px"}} src={el.thumbnail} alt="thumbnail" />
+              </div>
+              </div>
+            ))
+          }
         </div>
         <div>
           <div style={{ position: 'relative', right: '0' }} className="discover">
