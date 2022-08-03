a83123524a4d4e16e08ce7da019b1f8c7d0243b3
AnkitAnkushe <ankitankushe@gmail.com>
2022-08-03 14:13:14 +0530

    added login page

---
+++ src/components/Home_BL/Home.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './HomeBL.css'
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Login from '../gAuth/Login';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import EmailIcon from '@mui/icons-material/Email';
+import Box from '@mui/material/Box';
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const Home = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <div style={{ backgroundColor: "#ffc017" }}>
+        <Navbar />
+        <div className='Home_BL_Banner'>
+          <div style={{ paddingLeft: '120px' }}>
+            <h1 style={{ fontSize: "100px", fontWeight: "500" }}>Stay curious.</h1>
+            <p style={{ fontSize: "30px", fontWeight: "500", color: "black" }}>
+              Discover stories, thinking, and expertise
+              <br />
+              from writers on any topic.
+            </p>
+            <br />
+            <button onClick={handleOpen} style={{ fontSize: "20px", backgroundColor: "black", color: "white", borderRadius: "30px", height: "45px", width: "200px" }}>
+              Start Reading</button>
+          </div>
+          <div className='mmm'>
+            <img src="./mm.png" alt="" />
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='trending_home'>
+        <img src="./trending.png" alt="trendingImg" />
+      </div>
+      <hr />
+
+
+      <div className='home_bl_bottom'>
+          <div></div>
+          <div>
+          <div style={{ position: 'relative', right: '0' }} className="discover">
+          <div style={{ position: 'sticky', top: '110px' }}>
+            <h4>DISCOVER MORE OF WHAT MATTERS TO YOU</h4>
+
+            <div className="explore">
+              <button>Self</button>
+              <button>Relationships</button>
+              <button>Data Science</button>
+              <button>Programming</button>
+              <button>Productivity</button>
+              <button>Javascript</button>
+              <button>Development</button>
+              <button>Politics</button>
+              <button>Health</button>
+            </div>
+
+            <div style={{ width: '100%', gap: '15px' }} className="info">
+              <p>Help</p>
+              <p>Status</p>
+              <p>Writers</p>
+              <p>Blog</p>
+              <p>Careers</p>
+              <p>Privacy</p>
+              <p>Terms</p>
+              <p>About</p>
+            </div>
+          </div>
+          </div>
+          </div>
+        </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} style={{ height: "90%", width: "35%", textAlign: "center", border: "1px solid #cecece" }} >
+          <br /><br /><br /><br />
+          <h1 style={{ fontWeight: "400" }}>Join Medium.</h1>
+          <br /><br /><br />
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <Login />
+          </Typography>
+          <br />
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <button style={{ padding: "2%", width: "70%", backgroundColor: "white", border: "1px solid #cecece", color: "grey", fontSize: "14px", textAlign: "left" }}><FacebookIcon color='primary' /> Sign up with Facebook</button>
+          </Typography>
+          <br />
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <button style={{ padding: "2%", width: "70%", backgroundColor: "white", border: "1px solid #cecece", color: "grey", fontSize: "14px", textAlign: "left" }}><EmailIcon /> Sign up with Email</button>
+          </Typography>
+          <br />
+          <Typography id="modal-modal-description" sx={{ mt: 2 }} style={{ color: "grey", fontSize: "14px" }}>
+            Click “Sign Up” to agree to Medium’s Terms of Service and acknowledge that Medium’s Privacy Policy applies to you..
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+
+  )
+}
+
+export default Home
---
+++ src/components/SideNavbar.jsx
@@ -0,0 +1,135 @@
+import EditIcon from '@mui/icons-material/Edit';    
+import HomeIcon from '@mui/icons-material/Home';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import React, { useState } from 'react'
+import ArticleIcon from '@mui/icons-material/Article';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import BookmarksOutlinedIcon from '@mui/icons-material/BookmarksOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import { Button, Popover } from '@mui/material';
+
+export const SideNavbar = () => {
+    const [temp,setTemp] = useState(false);
+    const [home, setHome] = useState(false);
+    const [noti, setNoti] = useState(false);
+    const [book, setBook] = useState(false);
+    const [article, setArticle] = useState(false);
+    const [edit, setEdit] = useState(false);
+
+
+    const handleIcon = (iconName) => {
+        switch (iconName) {
+            case "home":
+                setHome(true)
+                setNoti(false)
+                setBook(false)
+                setArticle(false)
+                setEdit(false)
+                break;
+            case "noti":
+                setHome(false)
+                setNoti(true)
+                setBook(false)
+                setArticle(false)
+                setEdit(false)
+                break;
+            case "book":
+                setHome(false)
+                setNoti(false)
+                setBook(true)
+                setArticle(false)
+                setEdit(false)
+                break;
+            case "article":
+                setHome(false)
+                setNoti(false)
+                setBook(false)
+                setArticle(true)
+                setEdit(false)
+                break;
+            case "edit":
+                setHome(false)
+                setNoti(false)
+                setBook(false)
+                setArticle(false)
+                setEdit(true)
+                break;
+
+            default:
+                break;
+        }
+    }
+    const [anchor,setAnchor] = useState(null);
+    const openPopover = (event) =>{
+        setAnchor(event.currentTarget);
+        console.log("yash");
+    }
+
+    return (
+        <div style={{ width: "5.6%", borderRight: "1px solid gainsboro", position: "fixed", backgroundColor: "white", height: "50rem" }}>
+            <div style={{ padding: "20%", marginBottom: "7rem", marginTop: "2rem" }}>
+                <svg viewBox="0 0 1043.63 592.71" className="kp hu" style={{ width: "80%" }}><g data-name="Layer 2"><g data-name="Layer 1"><path d="M588.67 296.36c0 163.67-131.78 296.35-294.33 296.35S0 460 0 296.36 131.78 0 294.34 0s294.33 132.69 294.33 296.36M911.56 296.36c0 154.06-65.89 279-147.17 279s-147.17-124.94-147.17-279 65.88-279 147.16-279 147.17 124.9 147.17 279M1043.63 296.36c0 138-23.17 249.94-51.76 249.94s-51.75-111.91-51.75-249.94 23.17-249.94 51.75-249.94 51.76 111.9 51.76 249.94"></path></g></g></svg>
+            </div>
+            <div>
+                <div style={{ marginBottom: "1.5rem" }}>
+                    <button style={{ backgroundColor: "white", border: "none" }} onClick={() => handleIcon("home")}>
+                        {home ? <HomeIcon /> : <HomeOutlinedIcon />}
+                    </button>
+                </div>
+                <div style={{ marginBottom: "1.5rem" }}>
+                    <button style={{ backgroundColor: "white", border: "none" }} onClick={() => handleIcon("noti")}>
+                        {noti ? <NotificationsActiveIcon /> : <NotificationsActiveOutlinedIcon />}
+                    </button>
+                </div>
+                <div style={{ marginBottom: "1.5rem" }}>
+                    <button style={{ backgroundColor: "white", border: "none" }} onClick={() => handleIcon("book")}>
+                        {book ? <BookmarksIcon /> : <BookmarksOutlinedIcon />}
+                    </button>
+
+                </div>
+                <div style={{ marginBottom: "1.5rem" }}>
+                    <button style={{ backgroundColor: "white", border: "none" }} onClick={() => handleIcon("article")}>
+                        {article ? <ArticleIcon /> : <ArticleOutlinedIcon />}
+                    </button>
+                </div>
+                <div style={{ marginBottom: "1.5rem" }}>
+                    <button style={{ backgroundColor: "white", border: "none" }} onClick={() => handleIcon("edit")}>
+                        {edit ? <EditIcon /> : <EditOutlinedIcon />}
+                    </button>
+
+                </div>
+            </div>
+            <div style={{ marginBottom: "1.5rem" }}>
+                <HorizontalRuleIcon style={{ color: "grey" }} />
+            </div>
+            <div style={{ marginBottom: "1.5rem" }}>
+                <DriveFileRenameOutlineIcon />
+            </div>
+            <Popover
+            open={Boolean(anchor)}
+            anchorEl={anchor} 
+            anchorOrigin={{
+                vertical:"bottom",
+                horizontal:"right"
+            }}
+            transformOrigin={{
+                vertical:"bottom",
+                horizontal:"left"
+            }}
+            >
+                <Button variant='Danger' onClick={()=>setAnchor(null)}>Sign out</Button>
+            </Popover>
+            <button style={{ backgroundColor: "white", border: "none" }} onClick={openPopover}>
+                <div style={{ marginTop: "6rem" }}>
+                    <img style={{ width: "35%", borderRadius: "100%" }} src="https://www.jetsetter.com//uploads/sites/7/2019/04/GettyImages-920879930-1380x1380.jpg" alt="" />
+                </div>
+            </button>
+
+        </div>
+    )
+}
